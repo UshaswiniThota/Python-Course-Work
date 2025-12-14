55d4325b4812150850a18f6a96d3fd91eba2e43f
ushaswinithota <ushaswinithoa11@gmail.com>
2025-12-14 10:52:44 +0530

    folder created

--- Practice Programs/demo.py
+++ Practice Programs/demo.py
@@ -25,3 +25,38 @@ TypeError: 'tuple' object is not callable
 (1, 2, 3, 1, 2, 3, 1, 2, 3)
 >>> 4 in t
 False
+i in t
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    i in t
+NameError: name 'i' is not defined
+>>> l in t
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    l in t
+NameError: name 'l' is not defined
+>>> 1 in t
+True
+>>> lang=('java','python','c','c++','javascript','css','bootstrap')
+>>> lang[3]
+'c++'
+>>> lang[0]
+'java'
+>>> lang[-3]
+'javascript'
+>>> lang[:3]
+('java', 'python', 'c')
+>>> lang[-3:]
+('javascript', 'css', 'bootstrap')
+>>> lang[3:5]
+('c++', 'javascript')
+>>> lang[::2]
+('java', 'c', 'javascript', 'bootstrap')
+>>> t=(1,2,3)
+>>> a,b,c=t
+>>> a
+1
+>>> b
+2
+>>> c
+3
